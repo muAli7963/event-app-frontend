2e69a7c8924f9a5b5d99f2e1ed69b9ee8d906f8c
muAli <elshrab90.ma@gmail.com>
2021-10-31 18:06:19 +0200

    about adding functionality

--- components/EventItem.js
+++ components/EventItem.js
@@ -6,7 +6,7 @@ export default function EventItem(){
   return(
     <div className={styles.event}>
       <div className={styles.img}>
-        <Image src={evt.image ? evt.image:'/images/event-default.png' } width={170} height={100} />
+        <Image src={evt.image ? evt.image.formats.thubmnail.url:'/images/event-default.png' } width={170} height={100} />
         }
       </div>
       <div className={styles.link}>
